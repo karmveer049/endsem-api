7bcdad49457f5790151ac7453e5e1d8df9ec4b93
Karmveer <23bee050@nith.ac.in>
2026-02-15 19:20:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
+
 
 # load weights once
 with open("weights.json","r") as f:
